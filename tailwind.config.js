a998152d61729a3bdabd65b0bb3b907087d5afb7
rickie-ui <muciraerick254@gmail.com>
2023-07-07 05:22:54 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/images/one.jpg')",
+        'construction-pattern': "url('/images/construction.jpg')",
+        'about-pattern': "url('/images/three.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
